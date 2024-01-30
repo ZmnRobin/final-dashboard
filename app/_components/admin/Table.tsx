62cb59081e27488764e28a6b2deb7dedd3b56066
zmnrobin <robinmilford38@gmail.com>
2024-01-30 20:07:38 +0600

    added sadly commnet in admin dashboard

--- app/_components/admin/Table.tsx
+++ app/_components/admin/Table.tsx
@@ -1,12 +1,16 @@
-'use client'
-import React from 'react';
-import Image from 'next/image';
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
-import Role from './Role';
-import { formatDateToLocal, formatCurrency } from '../../_lib/utils/utilityFunction';
-import Link from 'next/link';
-import { deleteSingleUser } from '../../_lib/apiCall/admin/userApi';
-import Loader from '../common/Loader';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+import Role from "./Role";
+import {
+  formatDateToLocal,
+  formatCurrency,
+} from "../../_lib/utils/utilityFunction";
+import Link from "next/link";
+import { deleteSingleUser } from "../../_lib/apiCall/admin/userApi";
+import Loader from "../common/Loader";
+import toast from "react-hot-toast";
 
 interface User {
   _id: string;
@@ -20,14 +24,19 @@ interface TableProps {
   userList: User[];
   setUserList: React.Dispatch<React.SetStateAction<User[]>>;
   openUpdateModal: (userId: string) => void;
-  loading:boolean;
-  setLoading:React.Dispatch<React.SetStateAction<boolean>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Table: React.FC<TableProps> = ({ userList, setUserList, openUpdateModal,loading,setLoading }) => {
-
-  const handleDeleteUser = async (userId: string) => {
-    setLoading(true)
+const Table: React.FC<TableProps> = ({
+  userList,
+  setUserList,
+  openUpdateModal,
+  loading,
+  setLoading,
+}) => {
+  const handleDeleteUser = async (userId: string, name: string) => {
+    setLoading(true);
     try {
       // Delete user from the database
       await deleteSingleUser(userId);
@@ -35,9 +44,10 @@ const Table: React.FC<TableProps> = ({ userList, setUserList, openUpdateModal,lo
       // Update the UI by removing the deleted user from the state
       const updatedUserList = userList.filter((user) => user._id !== userId);
       setUserList(updatedUserList);
-      setLoading(false)
+      setLoading(false);
+      toast.error(`Sadly you deleted ${name}.`);
     } catch (error) {
-      console.error('Error deleting user:', error);
+      console.error("Error deleting user:", error);
       // Handle error here (e.g., display error message)
     }
   };
@@ -66,53 +76,70 @@ const Table: React.FC<TableProps> = ({ userList, setUserList, openUpdateModal,lo
                 </th> */}
               </tr>
             </thead>
-            {loading ? (<Loader/>):(
-            <tbody className="bg-white">
-              {userList?.map((user) => (
-                <tr
-                  key={user._id}
-                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
-                >
-                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex items-center gap-3">
-                      <Image
-                        src="/human.jpg"
-                        className="rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${user.name}'s profile picture`}
-                      />
-                      <p>{user.name}</p>
-                    </div>
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {user.phone}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    <Role role={user?.role} />
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {formatDateToLocal(user.createdAt,'en-US')}
-                  </td>
+            {loading ? (
+              <Loader />
+            ) : (
+              <tbody className="bg-white">
+                {userList?.map((user) => (
+                  <tr
+                    key={user._id}
+                    className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  >
+                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                      <div className="flex items-center gap-3">
+                        <Image
+                          src="/human.jpg"
+                          className="rounded-full"
+                          width={28}
+                          height={28}
+                          alt={`${user.name}'s profile picture`}
+                        />
+                        <p>{user.name}</p>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      {user.phone}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      <Role role={user?.role} />
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-3">
+                      {formatDateToLocal(user.createdAt, "en-US")}
+                    </td>
 
-                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex justify-end gap-3">
-                      <Link href="#" className="rounded-md border p-2 hover:bg-gray-100">
-                        <PencilIcon className="w-5" onClick={() => openUpdateModal(user._id)} />
-                      </Link>
-                      <Link href="#" className="rounded-md border p-2 hover:bg-gray-100">
-                        <TrashIcon className="w-5" onClick={() => handleDeleteUser(user._id)} />
-                      </Link>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>)}
+                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                      <div className="flex justify-end gap-3">
+                        <Link
+                          href="#"
+                          className="rounded-md border p-2 hover:bg-gray-100"
+                        >
+                          <PencilIcon
+                            className="w-5"
+                            onClick={() => openUpdateModal(user._id)}
+                          />
+                        </Link>
+                        <Link
+                          href="#"
+                          className="rounded-md border p-2 hover:bg-gray-100"
+                        >
+                          <TrashIcon
+                            className="w-5"
+                            onClick={() =>
+                              handleDeleteUser(user._id, user.name)
+                            }
+                          />
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            )}
           </table>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Table;
--- app/_components/admin/UserForm.tsx
+++ app/_components/admin/UserForm.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { createUser, updateUser, findSingleUser } from '../../_lib/apiCall/admin/userApi';
-import { XCircleIcon } from '@heroicons/react/24/outline';
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  createUser,
+  updateUser,
+  findSingleUser,
+} from "../../_lib/apiCall/admin/userApi";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import toast from "react-hot-toast";
 
 interface UserFormProps {
   closeModal: () => void;
@@ -10,7 +15,12 @@ interface UserFormProps {
   userId: string;
 }
 
-const UserForm: React.FC<UserFormProps> = ({ closeModal, fetchData, isUpdateMode, userId }) => {
+const UserForm: React.FC<UserFormProps> = ({
+  closeModal,
+  fetchData,
+  isUpdateMode,
+  userId,
+}) => {
   const {
     register,
     handleSubmit,
@@ -32,7 +42,7 @@ const UserForm: React.FC<UserFormProps> = ({ closeModal, fetchData, isUpdateMode
             role: user.role,
           });
         } catch (error) {
-          console.error('Error fetching user data:', error);
+          console.error("Error fetching user data:", error);
           // Handle error here
         }
       };
@@ -41,79 +51,100 @@ const UserForm: React.FC<UserFormProps> = ({ closeModal, fetchData, isUpdateMode
     }
   }, [isUpdateMode, userId, reset]);
 
-  const onSubmit = async (data: { name: string; phone: string; role: string }) => {
-    setLoading(true)
-    setError('')
+  const onSubmit = async (data: {
+    name: string;
+    phone: string;
+    role: string;
+  }) => {
+    setLoading(true);
+    setError("");
     try {
       if (isUpdateMode) {
         // Update existing user
-        await updateUser(userId, data).then(()=>{
-          fetchData()
-          setLoading(false)
-          closeModal()
+        await updateUser(userId, data).then(() => {
+          fetchData();
+          setLoading(false);
+          closeModal();
+          toast.success("User updated successfully.", { duration: 2000 });
         });
       } else {
         // Create new user
-        const userData={
+        const userData = {
           name: data?.name,
           phone: data?.phone,
           role: data?.role,
           password: data?.phone,
-        }
+        };
         await createUser(userData).then(() => {
           fetchData();
-          setLoading(false) // Fetch updated user list
+          setLoading(false); // Fetch updated user list
           closeModal();
+          toast.success("User added successfully.", { duration: 2000 });
         });
       }
     } catch (error) {
-      console.error('Error handling user data:', error);
-      setError('An error occurred while creating the new user!');
+      console.error("Error handling user data:", error);
+      setError("An error occurred while creating the new user!");
     }
   };
-
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-8 rounded-lg w-96">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <span className="text-red-600 cursor-pointer flex justify-end" onClick={closeModal}>
-            <XCircleIcon className='w-6'/>
+          <span
+            className="text-red-600 cursor-pointer flex justify-end"
+            onClick={closeModal}
+          >
+            <XCircleIcon className="w-6" />
           </span>
           <div className="mb-4">
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
               Name
             </label>
             <input
-              {...register('name', { required: true })} // Register name field
+              {...register("name", { required: true })} // Register name field
               type="text"
               id="name"
               name="name"
               className="mt-1 p-2 w-full border rounded-md"
               placeholder="user name"
             />
-            {errors.name && <p className="text-red-500 text-sm">{errors.name.message}</p>}
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name.message}</p>
+            )}
           </div>
           <div className="mb-4">
-            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
               Phone
             </label>
             <input
-              {...register('phone', { required: true })} // Register phone field
+              {...register("phone", { required: true })} // Register phone field
               type="tel"
               id="phone"
               name="phone"
               className="mt-1 p-2 w-full border rounded-md"
               placeholder="+8801234567890"
             />
-            {errors.phone && <p className="text-red-500 text-sm">{errors.phone.message}</p>}
+            {errors.phone && (
+              <p className="text-red-500 text-sm">{errors.phone.message}</p>
+            )}
           </div>
           <div className="mb-4">
-            <label htmlFor="role" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="role"
+              className="block text-sm font-medium text-gray-700"
+            >
               Role
             </label>
             <select
-              {...register('role', { required: true })} // Register role field
+              {...register("role", { required: true })} // Register role field
               id="role"
               name="role"
               className="mt-1 p-2 w-full border rounded-md"
@@ -121,15 +152,28 @@ const UserForm: React.FC<UserFormProps> = ({ closeModal, fetchData, isUpdateMode
               <option defaultValue="manager">Manager</option>
               <option value="seller">Seller</option>
             </select>
-            {errors.role && <p className="text-red-500 text-sm">{errors.role.message}</p>}
+            {errors.role && (
+              <p className="text-red-500 text-sm">{errors.role.message}</p>
+            )}
           </div>
           {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
-          {isUpdateMode ? <button disabled={loading} type="submit" className="bg-blue-600 text-white px-4 py-2 rounded-md w-full">
-           {loading ? 'Updating user, please wait ...' : 'Update User'}
-          </button>:
-          <button disabled={loading} type="submit" className="bg-blue-600 text-white px-4 py-2 rounded-md w-full">
-            {loading ? 'Creating user, please wait ...' : 'Create User'}
-          </button>}
+          {isUpdateMode ? (
+            <button
+              disabled={loading}
+              type="submit"
+              className="bg-blue-600 text-white px-4 py-2 rounded-md w-full"
+            >
+              {loading ? "Updating user, please wait ..." : "Update User"}
+            </button>
+          ) : (
+            <button
+              disabled={loading}
+              type="submit"
+              className="bg-blue-600 text-white px-4 py-2 rounded-md w-full"
+            >
+              {loading ? "Creating user, please wait ..." : "Create User"}
+            </button>
+          )}
         </form>
       </div>
     </div>

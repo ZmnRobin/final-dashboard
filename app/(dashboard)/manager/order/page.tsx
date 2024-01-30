a9326acc2a5c8bedaadf6510f0d35e0ef65c158c
zmnrobin <robinmilford38@gmail.com>
2024-01-30 16:25:13 +0600

    update manager dashboard

--- app/(dashboard)/manager/order/page.tsx
+++ app/(dashboard)/manager/order/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+"use client";
+import OrderTable from "@/app/_components/pos/OrderTable";
+import { getAllOrder } from "@/app/_lib/apiCall/manager/orderApi";
+import { OrderType } from "@/app/_types/FoodItemTypes";
+import React, { useEffect, useState } from "react";
 
 export default function OrderPage() {
+  const [orderList, setOrderList] = useState<OrderType[]>([]); // Initialize userList state
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // Fetch user list when the component mounts
+  const fetchData = async () => {
+    setLoading(true); // Set loading to true before the API call
+    try {
+      const orders = await getAllOrder();
+      setOrderList(orders.reverse());
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+      // Handle error here (e.g., display error message)
+    } finally {
+      setLoading(false); // Set loading to false after the API call
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <h1 className={`mb-4 text-xl md:text-2xl`}>Order List</h1>
-  )
+    <main>
+      <OrderTable
+        orderList={orderList}
+        loading={loading}
+        setLoading={setLoading}
+        fetchData={fetchData}
+      />
+    </main>
+  );
 }
--- app/(dashboard)/seller/page.tsx
+++ app/(dashboard)/seller/page.tsx
@@ -3,18 +3,22 @@ import OrderTable from "@/app/_components/pos/OrderTable";
 import React, { useEffect, useState } from "react";
 import { getAllOrder } from "@/app/_lib/apiCall/manager/orderApi";
 import { OrderType } from "@/app/_types/FoodItemTypes";
+import { getNameFromCookies } from "@/app/_lib/utils/utilityFunction";
 
 export default function SellerPage() {
   const [orderList, setOrderList] = useState<OrderType[]>([]); // Initialize userList state
   const [loading, setLoading] = useState<boolean>(false);
+  const currentUserName = getNameFromCookies();
 
   // Fetch user list when the component mounts
   const fetchData = async () => {
     setLoading(true); // Set loading to true before the API call
     try {
-      const orders = await getAllOrder();
-      setOrderList(orders.reverse());
-      console.log(orders, orderList);
+      let orders = await getAllOrder();
+      const newOrder = orders?.filter(
+        (order: any) => order.issuedBy === currentUserName
+      );
+      setOrderList(newOrder.reverse());
     } catch (error) {
       console.error("Error fetching orders:", error);
       // Handle error here (e.g., display error message)

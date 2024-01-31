dc40eeeeba2d7233012efbd22a97f8ae4cca6b85
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-01-31 17:54:47 +0600

    added conditional bg color in admin dashboard

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,22 +1,21 @@
-'use client'
-import React from 'react';
-import Card from '../../_components/admin/Card';
-import RevenueChart from '../../_components/admin/RevenueChart';
-import { revenue } from '@/app/_lib/data/data';
+"use client";
+import React from "react";
+import Card from "../../_components/admin/Card";
+import RevenueChart from "../../_components/admin/RevenueChart";
+import { revenue } from "@/app/_lib/data/data";
 // import WithAuth from '../../../_components/auth/WithAuth'
 
 function HomePage() {
   return (
     <main>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      <Card title="Collected" value="15" type="collected" />
-      <Card title="Pending" value="9" type="pending" />
-      <Card title="Total Invoices" value="15" type="invoices" />
-      <Card title="Total Customers" value="100" type="customers"
-      /> 
+        <Card title="Total Order" value="15" type="customers" />
+        <Card title="Completed" value="9" type="completed" />
+        <Card title="Cancelled" value="15" type="cancelled" />
+        <Card title="Total Bill" value="100" type="collected" />
       </div>
-        <div className="">
-          <RevenueChart revenue={revenue} />
+      <div className="">
+        <RevenueChart revenue={revenue} />
       </div>
     </main>
   );
--- app/_components/admin/Card.tsx
+++ app/_components/admin/Card.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import {
   BanknotesIcon,
-  ClockIcon,
+  CheckCircleIcon,
   UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
+  XCircleIcon,
+} from "@heroicons/react/24/outline";
 
 interface CardProps {
   title: string;
@@ -15,15 +16,31 @@ interface CardProps {
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
+  completed: CheckCircleIcon,
+  cancelled: XCircleIcon,
 };
 
 const Card: React.FC<CardProps> = ({ title, value, type }) => {
   const Icon = iconMap[type];
+  const [bgColor, setBgColor] = useState("gray");
+
+  useEffect(() => {
+    if (title === "Completed") {
+      setBgColor("green");
+    }
+    if (title === "Cancelled") {
+      setBgColor("red");
+    }
+    if (title === "Total Order") {
+      setBgColor("gray");
+    }
+    if (title === "Total Bill") {
+      setBgColor("gray");
+    }
+  }, [title]);
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+    <div className={`rounded-xl bg-${bgColor}-100 p-2 shadow-sm`}>
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>

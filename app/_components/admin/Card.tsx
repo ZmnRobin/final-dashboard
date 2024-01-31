bb2308219b940a915219b5a67827dcfb43c9b712
zmnrobin <robinmilford38@gmail.com>
2024-01-31 20:13:02 +0600

    updated all file

--- app/_components/admin/Card.tsx
+++ app/_components/admin/Card.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
+import React from "react";
 import {
   BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
+  XMarkIcon,
+  CheckIcon,
+} from "@heroicons/react/24/outline";
 
 interface CardProps {
   title: string;
   value: string;
+  color: string;
   type: keyof typeof iconMap;
 }
 
 const iconMap = {
-  collected: BanknotesIcon,
-  customers: UserGroupIcon,
-  pending: ClockIcon,
-  invoices: InboxIcon,
+  totalOrder: BanknotesIcon,
+  approved: CheckIcon,
+  cancelled: XMarkIcon,
+  totalBill: BanknotesIcon,
 };
 
-const Card: React.FC<CardProps> = ({ title, value, type }) => {
+const Card: React.FC<CardProps> = ({ title, value, type, color }) => {
   const Icon = iconMap[type];
-
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+    <div
+      className={`rounded-xl bg-${color ? color : "gray"}-300 p-2 shadow-sm`}
+    >
       <div className="flex p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
